8e0c5a5349c14498789f497bc9221391d8abbb7d
ConradsMaia <conrado.alves1993@gmail.com>
2021-09-13 09:25:00 -0300

    listMoney

---
+++ corpo.js
@@ -0,0 +1,7 @@
+function atribuir() {
+    var sal = window.document.querySelector('input#salario')
+    var destinoo = window.document.querySelector('div#destino')
+    var total = Number (sal.value)
+    destino.innerHTML = `seu total é de ${total}`
+   
+}
